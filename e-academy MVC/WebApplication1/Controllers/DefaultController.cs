93c014ad051fe7c154bc83d300c51a60a04525e8
Osman Ata <bizbize_yeterizkv@hotmail.com>
2022-12-29 23:38:52 +0300

    Html MVC yapısına çevirme
    c++ ile MVC modeline çevirme

---
+++ e-academy MVC/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route ("Hakkinda")]
+        public ActionResult Hakkinda()
+        {
+            using(eacademiEntities db = new eacademiEntities())
+            {
+                var model = db.hakkinda.Find(1);
+                return View (model);
+            }
+           
+        }
+        [Route("Dersler")]
+        public ActionResult Dersler()
+        {
+            using (eacademiEntities db = new eacademiEntities())
+            {
+                var model = db.dersler.ToList();
+                return View(model);
+            }
+        }
+        [Route("Blog")]
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        [Route("Iletisim")]
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
